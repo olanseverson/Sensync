6281f6d37a51aafe16f1c7e78ff86f0e91705d3e
olanseverson <olanseverson13@gmail.com>
2019-05-07 07:54:33 +0700

    add documentation

--- init/init.ino
+++ init/init.ino
@@ -1,11 +1,41 @@
+/*
+  ||
+  || @file init.ino
+  || @version 1.0
+  || @author Yoland Nababan
+  ||
+  || @description
+  || | Data logging project.
+  || | Act as a slave to a Raspberry using I2C Communication. Arduino Send 4 value when request from Master (Raspberry)
+  || | The data is Analog Value from 4 different potentiometer. Data is uploaded to ThingsSpeak.com to store the data.
+  || | 4 analog data store in 1 Channel, 4 field data. To store data to internet, see main program in python (.py).
+  || #
+  ||
+*/
+
+/* I2C FOR SERIAL COMMUNICATION
+ * Connection:
+ * ARDUINO ============ RASPI (PI 3 MODEL B V1.2)
+ * SDA(A4) ===========> SDA(BCM 2)
+ * SCL(A5) ===========> SCL(BCM 3)
+ * GND     ===========> GND
+*/
+
+/** POTENTIOMETER
+ * using analog(pin 14) A0 to A3(pin 17)
+ */
+
+//for I2C library 
 #include <Wire.h>
 
-#define SLAVE_ADDRESS 0x10
-int number = 0;
-int state = 0;
-int sensorValue[4] = {0, 0, 0, 0};
-word sensorPin[4] = {14, 15, 16, 17};
+#define SLAVE_ADDRESS 0x10 //address of Arduino seen by Raspi
+
+int number = 0; // variable dummy to store data sent by Raspi 
+
+int sensorValue[4] = {0, 0, 0, 0}; // Raw ADC data
+word sensorPin[4] = {14, 15, 16, 17}; // pin ADC
 void setup() {
+  
   pinMode(13, OUTPUT);
   Serial.begin(9600); // start serial for output
   // initialize i2c as slave
@@ -13,7 +43,7 @@ void setup() {
 
   // define callbacks for i2c communication
   Wire.onReceive(receiveData);
-  Wire.onRequest(sendData);
+  Wire.onRequest(sendData);s
 
   Serial.println("Ready!");
 }
@@ -22,29 +52,45 @@ void loop() {
   delay(100);
 }
 
-// callback for received data
-void receiveData(int byteCount) {
 
-  while (Wire.available()) {
+void receiveData(int byteCount) 
+/** callback for received data
+ * update the value of adc every times Raspi (MASTER) request to arduino
+ */
+ 
+{
+  while (Wire.available()) { // get the data from raspi that has been stored in buffer
     number = Wire.read();
     Serial.print("data received: ");
     Serial.println(number);
   }
-  for (int i = 0; i < 4; i++) {
+  
+  for (int i = 0; i < 4; i++) { // update the value of adc
     sensorValue[i] = analogRead(sensorPin[i]);
     Serial.println(sensorValue[i]);
   }
   Serial.println("--");
 }
 
-// callback for sending data
-void sendData() {
-  
+
+void sendData() 
+/** callback for sending data
+ * sending the updated ADC value back to Raspi(MASTER)
+ * ADC is 10 bit, but Byte data is 8 bit, so data must be separated to 2 variabel (BYTES) before sending to raspi
+ * in main program (RASPI), the separated data must be combine again.
+ */
+{
   byte x[8];
-  for (int i = 0; i < 4; i++) {
+  for (int i = 0; i < 4; i++) { //separating 10 bit ADC to 2-8 bit for transfering data
     x[2*i] = sensorValue[i]>>8; // msb
     x[(2*i) + 1] = sensorValue[i]&0x00ff; // lsb
   }
   
-  Wire.write(x, 8);
+  Wire.write(x, 8); // write 8 bytes of data from 4 ADC value
 }
+
+/*
+  || @changelog
+  || | 1.0 2019-05-08 - Yoland Nababan : Initial Release
+  || #
+*/
--- init/init.py
+++ init/init.py
@@ -1,8 +1,31 @@
-# https://www.waveshare.com/wiki/Raspberry_Pi_Tutorial_Series:_I2C
-# https://pydigger.com/pypi/smbus2
-# https://smbus2.readthedocs.io/en/latest/
-# https://electronicshobbyists.com/raspberry-pi-sending-data-to-thingspeak-simplest-raspberry-pi-iot-project/
-# https://www.hackster.io/adamgarbo/raspberry-pi-2-iot-thingspeak-dht22-sensor-b208f4
+#~ /*
+  #~ || @file init.py
+  #~ || @version 1.0
+  #~ || @author Yoland Nababan
+  #~ ||
+  #~ || @description
+  #~ || | Data logging project.
+  #~ || | Act as a master to a arduino using I2C Communication. Arduino Send 4 value when request from Master (Raspberry)
+  #~ || | The data is Analog Value from 4 different potentiometer. Data is uploaded to ThingsSpeak.com to store the data.
+  #~ || | 4 analog data store in 1 Channel, 4 field data. This program is the interface to internet.
+  #~ || #
+  #~ || 
+  #~ || @documentation
+  #~ || | https://www.waveshare.com/wiki/Raspberry_Pi_Tutorial_Series:_I2C
+  #~ || | https://pydigger.com/pypi/smbus2
+  #~ || | https://smbus2.readthedocs.io/en/latest/
+  #~ || | https://electronicshobbyists.com/raspberry-pi-sending-data-to-thingspeak-simplest-raspberry-pi-iot-project/
+  #~ || | https://www.hackster.io/adamgarbo/raspberry-pi-2-iot-thingspeak-dht22-sensor-b208f4
+  #~ || #
+#~ */
+
+#~ /* I2C FOR SERIAL COMMUNICATION
+ #~ * Connection:
+ #~ * ARDUINO ============ RASPI (PI 3 MODEL B V1.2)
+ #~ * SDA(A4) ===========> SDA(BCM 2)
+ #~ * SCL(A5) ===========> SCL(BCM 3)
+ #~ * GND     ===========> GND
+#~ */
 
 import time
 import numpy as np
@@ -10,15 +33,16 @@ from smbus2 import SMBusWrapper, i2c_msg
 import sys 
 import urllib2 
 
-myAPI = '3JEXAHO11O1HUNA3'
+myAPI = '3JEXAHO11O1HUNA3' # API for ThingSpeak  (setting the permission first)
 
 # This is the address we setup in the Arduino Program
 address1 = 0x10
 
-# Delay time
-periods = 5
+# Delay time (in second)
+periods = 60
 
 def writeNumber(address, value):
+# write <list> value to slave to the address
     with SMBusWrapper(1) as bus:        
         # Write some bytes to address 
         msg = i2c_msg.write(address, value)
@@ -26,17 +50,18 @@ def writeNumber(address, value):
     return -1
 
 def readNumber(address, length):
+# read 'length' bytes from slave using address and store value to array
     with SMBusWrapper(1) as bus:
         # Read 'length' bytes from 'address'
         msg = i2c_msg.read(address, length)
         bus.i2c_rdwr(msg)
-        i = 0;
         data = [] 
-        for value in msg:
+        for value in msg: # store the requested data to array
             data.append(value)
     return data
 
 def toWord(msb, lsb):
+# combine msb(8bytes) and lsb(8 bytes) to num
     num = (msb<<8)|lsb
     return num
 
@@ -46,23 +71,20 @@ def main():
     baseURL = 'https://api.thingspeak.com/update?api_key=%s' % myAPI 
     
     while True:
-        #~ var = input("Enter 1 to 9: ")
-        #~ if not var:
-            #~ continue
         try: 
             # Send dummy value, to trigger arduino to capture data
             dummy = [0]
-            writeNumber(address1, dummy) 
+            writeNumber(address1, dummy) # the arduino respond this by updating analogRead
             print "RPI: Hi Arduino, I sent you ", dummy
             # sleep one second
             time.sleep(1)
             
             # Get data from Arduino
             data = []
-            raw = readNumber(address1, 8)
+            raw = readNumber(address1, 8)  # receive 8 bytes data (each 2 bytes represent 1 sensorValue)
             print "Arduino: Hey RPI, I received a digit ", raw
             for i in range(0, 4):
-                data.append(toWord(raw[i*2], raw[(i*2) + 1]))
+                data.append(toWord(raw[i*2], raw[(i*2) + 1]))  # combine 8 bytes to 4 bytes (becomes original data)
             print data 
              
             # 'data' will be sent to internet
@@ -75,5 +97,11 @@ def main():
             break 
         
 
-if __name__ == '__main__':
-    main()
+if __name__ == '__main__': 
+    main() # main program
+
+#~ /*
+  #~ || @changelog
+  #~ || | 1.0 2019-05-08 - Yoland Nababan : Initial Release
+  #~ || #
+#~ *
