a94a2c40542c7c635ffb61946eac486d0ad10e75
olanseverson <olanseverson13@gmail.com>
2019-06-17 18:29:28 +0700

    sparing can upload to server

--- sparing/reading/reading.ino
+++ sparing/reading/reading.ino
@@ -1,15 +1,162 @@
 #include <ModbusMaster.h>
-
+#include <SoftwareSerial.h>
 /*!
   We're using a MAX485-compatible RS485 Transceiver.
   Rx/Tx is hooked up to the hardware serial port at 'Serial'.
   The Data Enable and Receiver Enable pins are hooked up as follows:
 */
-#define MAX485_DE      3
-#define MAX485_RE_NEG  2
+
+//============== TSS ===================
+#define rx 11                                          //define what pin rx is going to be
+#define tx 10                                          //define what pin tx is going to be
+SoftwareSerial mySerial(rx, tx);
+
+
+#define MAX485_DE      9
+#define MAX485_RE_NEG  12
 
 // instantiate ModbusMaster object
 ModbusMaster node;
+//======================================
+
+//================ PH SENSOR ===========
+#define Offset 0.00            //deviation compensate
+#define SensorPin A0            //pH meter Analog output to Arduino Analog Input 0
+//======================================
+
+
+//================ WeMos ===============
+#define Idle 0
+#define SENDING 1
+#define WAITING 2
+#define RECEIVING 3
+
+enum upload_status {
+  idle = Idle,
+  sending = SENDING,
+  waiting = WAITING,
+  receiving = RECEIVING,
+};
+
+enum upload_status state = idle;
+//======================================
+
+void setup()
+{
+  pinMode(MAX485_RE_NEG, OUTPUT);
+  pinMode(MAX485_DE, OUTPUT);
+  // Init in receive mode
+  digitalWrite(MAX485_RE_NEG, 0);
+  digitalWrite(MAX485_DE, 0);
+
+  //debugging purposes
+  Serial.begin(19200);
+
+  // Modbus communication runs at 19200 baud
+  mySerial.begin(19200);
+
+  // WeMos communication
+  Serial1.begin(19200);
+
+  // Modbus slave ID 1
+  node.begin(10, mySerial);
+  // Callbacks allow us to configure the RS485 transceiver correctly
+  node.preTransmission(preTransmission);
+  node.postTransmission(postTransmission);
+
+  //WAITING FOR WEMOS TO CONNECT TO INTERNET
+  while(!Serial1.available()){ // empty the rx buffer
+    Serial.print(".");
+    delay(500);
+  }
+}
+
+void loop()
+{
+  float tss = getTSS(0x02);
+  float ph = getPH(10, SensorPin);
+  
+  String data = "";
+  data = String(ph)+ ",";
+  data = data + String(tss) + ",";
+  Serial.println(data);
+  echo(data);
+  delay(5000);
+}
+
+void echo(String data) {
+  while(Serial1.available()){ // empty the rx buffer
+    Serial1.read();
+  }
+  printState();
+  Serial1.print(data);
+  Serial1.flush();
+  state = sending;
+  printState();
+
+  while (!Serial1.available()) {
+    state = waiting;
+    printState();
+  }
+  
+  state = receiving;
+  printState();
+
+  String rawData;
+  while (Serial1.available()) {
+    char in = Serial1.read();
+    rawData += in;
+  }
+  Serial.println(rawData);
+  state = idle;
+}
+
+float getTSS(byte reg) {
+  uint8_t check;
+  uint32_t result = 0;
+  check = node.readHoldingRegisters(reg, 2);
+  if (check == node.ku8MBSuccess)
+  {
+    //getting raw data
+    uint32_t AB = node.getResponseBuffer(0x00);
+    uint32_t CD = node.getResponseBuffer(0x01);
+
+    //concat 2 x 16 bit to 32 bit;
+    result = (((0xffff & CD) << 16) | (0xffff & AB));
+
+    //debugging
+    //    float t = result;
+    //    byte* a;
+    //    a = (byte*) &t;
+    //    Serial.print(AB); Serial.print("       "); Serial.println(AB, BIN);
+    //    Serial.print(CD); Serial.print("       "); Serial.println(CD, BIN);
+    //    Serial.print(result); Serial.print("       "); Serial.println(result, HEX);
+    //    Serial.print(a[3], HEX);
+    //    Serial.print(a[2], HEX);
+    //    Serial.print(a[1], HEX);
+    //    Serial.print(a[0], HEX);
+    //    Serial.println();
+    //    Serial.println();
+  }
+
+  // convert to float
+  float* temp = (float*) &result;
+  return *temp;
+}
+
+float getPH(int arrLength, int analogPin) {
+  float avg = 0;
+  long amount = 0;
+  for (int i = 0; i < arrLength; i++) {
+    amount = amount + analogRead(analogPin);
+  }
+  avg = (float)amount / arrLength;
+  float voltage = avg * 5.0 / 1024;
+  float sensorValue = 3.5 * voltage + Offset;
+
+  return sensorValue;
+}
+
 
 void preTransmission()
 {
@@ -23,75 +170,21 @@ void postTransmission()
   digitalWrite(MAX485_DE, 0);
 }
 
-void setup()
-{
-  pinMode(MAX485_RE_NEG, OUTPUT);
-  pinMode(MAX485_DE, OUTPUT);
-  // Init in receive mode
-  digitalWrite(MAX485_RE_NEG, 0);
-  digitalWrite(MAX485_DE, 0);
-
-  // Modbus communication runs at 115200 baud
-  Serial.begin(19200);
-  Serial1.begin(19200);
-
-  // Modbus slave ID 1
-  node.begin(10, Serial1);
-  // Callbacks allow us to configure the RS485 transceiver correctly
-  node.preTransmission(preTransmission);
-  node.postTransmission(postTransmission);
-}
-
-bool state = true;
-
-void loop()
-{
-  uint8_t result;
-  uint16_t data[6];
-
-  // Toggle the coil at address 0x0002 (Manual Load Control)
-  //  result = node.writeSingleCoil(0x0002, state);
-  //  state = !state;
-
-  //100011000010001
-
-
-  // Read 16 registers starting at 0x3100)
-  result = node.readHoldingRegisters(0x02, 2);
-  if (result == node.ku8MBSuccess)
-  {
-
-
-    uint32_t AB = node.getResponseBuffer(0x00);
-    uint32_t CD = node.getResponseBuffer(0x01);
-    Serial.print(AB); Serial.print("       "); Serial.println(AB, BIN);
-    Serial.print(CD); Serial.print("       "); Serial.println(CD, BIN);
-    uint32_t x = (((0xffff&CD) << 16) | (0xffff & AB));
-
-    Serial.print(x); Serial.print("       "); Serial.println(x, HEX);
-    Serial.print(float(x)); Serial.print("       "); Serial.println(float(x), HEX);
-    float t = x;
-    byte* a;
-    a = (byte*) &t;
-    Serial.print(a[3], HEX);
-    Serial.print(a[2], HEX);
-    Serial.print(a[1], HEX);
-    Serial.print(a[0], HEX);
-    Serial.println();
-    Serial.println();
+void printState() {
+  switch (state) {
+    case Idle:
+      Serial.println("IDLE");
+      break;
+    case SENDING:
+      Serial.println("SENDING");
+      break;
+    case WAITING:
+      Serial.println("WAITING");
+      break;
+    case RECEIVING:
+      Serial.println("RECEIVING");
+      break;
 
   }
 
-  //  if (result == node.ku8MBSuccess)
-  //  {
-  //    Serial.print("Vbatt: ");
-  //    Serial.println(node.getResponseBuffer(0x00));
-  //    Serial.print("Vload: ");
-  //    Serial.println(node.getResponseBuffer(0x01));
-  //    Serial.print("Pload: ");
-  //    Serial.println((node.getResponseBuffer(0x0D) +
-  //                    node.getResponseBuffer(0x0E) << 16)/100.0f);
-  //  }
-
-  delay(1000);
 }
---
+++ sparing/uploading/uploading.ino
@@ -0,0 +1,121 @@
+//http://www.esp8266learning.com/dht11-sensor-data-to-thingspeak-using-a-wemos-d1.php
+#include <ESP8266WiFi.h>
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266HTTPClient.h>
+#include <SoftwareSerial.h>
+#define LED_PIN LED_BUILTIN   //D4
+
+//-- ESP_TX pin 14/D5 -  15/D8
+//-- ESP_RX pin 12/D6 -  13/D7
+SoftwareSerial ardSerial(14, 12, false, 256);
+
+// replace with your channel’s thingspeak API key and your SSID and password
+String apiKey = "3JEXAHO11O1HUNA3";
+const char* ssid = "Get Sensync";
+const char* password = "makanminggu12";
+const char* serverThingspeak = "api.thingspeak.com";
+
+const String url_upload   = "http://server.getsensync.com/proc/sparin/process.php?";
+const String param[2] = {"ph", "tss"};
+
+WiFiClient client;
+
+String rawData = "";
+int ledState = LOW;
+bool isUpdated = false;
+
+void setup() {
+  pinMode(LED_PIN, OUTPUT);
+  Serial.begin(19200);
+  ardSerial.begin(19200);
+  delay(500);
+
+  WiFi.begin(ssid, password);
+
+  Serial.println();
+  Serial.println();
+  Serial.print("Connecting to ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(100);
+    Serial.print(".");
+  }
+  Serial.println("");
+  Serial.println("WiFi connected");
+  ardSerial.print('\n'); // send a char to start program in Arduino
+}
+
+void loop() {
+  updateSerial();
+}
+
+void updateSerial()
+{
+  delay(100);
+  rawData = "";
+  if (ardSerial.available())
+  {
+    while (ardSerial.available()) {
+      char in = ardSerial.read();
+      rawData += in;
+    }
+    Serial.println(rawData);//Forward what Software Serial received to Serial Port
+
+    String data = parseData(rawData, ",", false);
+    Serial.println(data);
+    uploadToServer(data);
+  }
+}
+
+
+String parseData(String rawData, String delimiter, bool isToThingspeak) {
+  String temp = rawData; //copy rawData so rawData is not disappear
+  char count = '1';
+  int param_cnt = 0;
+  String outputData = ""; // data to be returned
+  int pos = 0;
+  String token;
+  while ( (pos = temp.indexOf(delimiter)) != -1) {
+    token = temp.substring(0, pos);  // copy string from index 0 to pos
+    if (isToThingspeak) {
+      outputData = outputData + "&field" + count + "=" + token;
+    }
+    else {
+      outputData = outputData + "&" + param[param_cnt] + "=" + token;
+    }
+    param_cnt++;
+    count ++;
+    temp.remove(0, pos + delimiter.length()); // remove string from index 0 to pos
+  }
+  return outputData;
+}
+
+
+
+void uploadToServer(String data) {
+
+  String datana = url_upload + data;
+
+  HTTPClient http;  //Declare object of class HTTPClient
+  http.begin( datana ); //Specify request destination
+  int httpCode    = http.GET(); //Send the request
+  String payload  = http.getString(); //Get the response payload
+  payload.trim();
+
+  Serial.println(datana);
+  Serial.print("HTTP return code: ");
+  Serial.println(httpCode);   //Print HTTP return code
+  Serial.print(F("Response: "));
+  Serial.println(payload);    //Print request response payload
+  yield();
+  http.end();     //Close connection
+
+  delay(1000);
+  ardSerial.print(httpCode);
+  ardSerial.flush();
+}
